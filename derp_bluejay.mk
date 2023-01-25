1490d7c4507246288e8ccc3fe9702c57c8f77ee7
stevn4127 <stevn4127@gmail.com>
2023-01-24 20:12:38 -0500

    Revert inline kernel build and rename for Derp

--- derp_bluejay.mk
+++ derp_bluejay.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bluejay/aosp_bluejay.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, device/google/bluejay/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6a
-PRODUCT_NAME := lineage_bluejay
+PRODUCT_NAME := derp_bluejay
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
